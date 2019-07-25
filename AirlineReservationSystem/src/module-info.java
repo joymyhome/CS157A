80d7ea86a5feef8355d0c99a9f03b5e5b7065ea4
Jing Z <faith.zjing@gmail.com>
2019-07-24 23:52:02 -0700

    Updated GUI files

---
+++ AirlineReservationSystem/src/module-info.java
@@ -0,0 +1,8 @@
+module AirlineReservationSystem {
+	requires java.desktop;
+	requires java.base;
+	requires java.logging;
+	requires java.sql;
+	requires mysql.connector.java;
+	requires jcalendar;
+}
