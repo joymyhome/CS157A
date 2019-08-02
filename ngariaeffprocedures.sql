401f8543528b598b1eb1876d12735008d19e32b0
ngariaeff <nicholas.gariaeff@sjsu.edu>
2019-08-01 22:04:50 -0700

    Add files via upload

--- ngariaeffprocedures.sql
+++ ngariaeffprocedures.sql
@@ -1,17 +1,18 @@
--- ADMIN: users who made a payment
+-- ADMIN: users who are above the average amount paid
 DROP PROCEDURE IF EXISTS userPayment;
 DELIMITER //
 CREATE PROCEDURE userPayment ()
 BEGIN
-	SELECT custom_id FROM customer, booking
-    WHERE customer.custom_id = booking.custom_id GROUP BY custom_id HAVING booking.payment > 0;
+	SELECT customer.custom_id, booking.payment FROM customer, booking
+    WHERE customer.custom_id = booking.custom_id GROUP BY customer.custom_id, booking.payment 
+    HAVING booking.payment > (SELECT AVG(payment) FROM booking);
 END //
 DELIMITER ;
 
 -- ADMIN: all users in database along with all users that are in booking
 DROP PROCEDURE IF EXISTS userBooked;
 DELIMITER //
-CREATE PROCEDURE userBooked ()
+CREATE PROCEDURE userBooked()
 BEGIN
 	SELECT custom_id AS CustomerID FROM customer
     UNION ALL
