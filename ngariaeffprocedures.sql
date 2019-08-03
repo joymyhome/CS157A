11c6e09fa153c5a08606b1656712f2cb796fea3c
ngariaeff <nicholas.gariaeff@sjsu.edu>
2019-08-02 18:20:07 -0700

    Updated second procedure

--- ngariaeffprocedures.sql
+++ ngariaeffprocedures.sql
@@ -9,14 +9,14 @@ BEGIN
 END //
 DELIMITER ;
 
--- ADMIN: users who are booked
+-- ADMIN: users who booked flight1 and flight2
 DROP PROCEDURE IF EXISTS userBooked;
 DELIMITER //
-CREATE PROCEDURE userBooked()
+CREATE PROCEDURE userBooked(in flightid1 int(10), in flightid2 int(10))
 BEGIN
 	SELECT customer.custom_id FROM customer WHERE customer.custom_id IN (
-	SELECT customer.custom_id FROM customer, booking WHERE customer.custom_id = booking.custom_id
+	SELECT booking.custom_id FROM booking WHERE booking.flight_id = flightid1
     UNION
-    SELECT booking.custom_id FROM customer, booking WHERE customer.custom_id = booking.custom_id);
+    SELECT booking.custom_id FROM booking WHERE booking.flight_id = flightid2);
 END //
 DELIMITER ;
